af8338952e26113b8a915c6204b322e4856c8d79
Mike <mikesmallhelp@gmail.com>
2022-10-26 17:06:49 +0300

    Toimiva välitallennus

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,60 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import React from "react"
+import { GetStaticProps } from "next"
+import Layout from "../components/Layout"
+import Post, { PostProps } from "../components/Post"
 
-export default function Home() {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = [
+    {
+      id: "1",
+      title: "Prisma is the perfect ORM for Next.js",
+      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+      published: false,
+      author: {
+        name: "Nikolas Burk",
+        email: "burk@prisma.io",
+      },
+    },
+  ]
+  return { 
+    props: { feed }, 
+    revalidate: 10 
+  }
 }
+
+type Props = {
+  feed: PostProps[]
+}
+
+const Blog: React.FC<Props> = (props) => {
+  return (
+    <Layout>
+      <div className="page">
+        <h1>Public Feed</h1>
+        <main>
+          {props.feed.map((post) => (
+            <div key={post.id} className="post">
+              <Post post={post} />
+            </div>
+          ))}
+        </main>
+      </div>
+      <style jsx>{`
+        .post {
+          background: white;
+          transition: box-shadow 0.1s ease-in;
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Blog
