56b0897915ad6bf7236e82ad2c98cecdfa1556ee
Mike <mikesmallhelp@gmail.com>
2022-11-02 16:13:08 +0200

    Responsiivisuus tuloksiin

--- pages/add-result.tsx
+++ pages/add-result.tsx
@@ -37,9 +37,9 @@ const TeamResult = ({ players, title }: { players: Player[], title: string }) =>
     return (
         <Grid container spacing={2} pl={2} pr={2} pb={2}>
             <Grid item xs={12}><Title>{title}</Title></Grid>
-            <Grid item xs={4}><PlayerSelect players={players} title="Pelaaja 1" /></Grid>
-            <Grid item xs={4}><PlayerSelect players={players} title="Pelaaja 2" /></Grid>
-            <Grid item xs={4}><PointsSelect title="Pisteet" /></Grid>
+            <Grid item xs={12} md={4}><PlayerSelect players={players} title="Pelaaja 1" /></Grid>
+            <Grid item xs={12} md={4}><PlayerSelect players={players} title="Pelaaja 2" /></Grid>
+            <Grid item xs={12} md={4}><PointsSelect title="Pisteet" /></Grid>
         </Grid>
     )
 }
