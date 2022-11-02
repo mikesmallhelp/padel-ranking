be4010dd9856c7615f0608dad3663418f9b7a531
Mike <mikesmallhelp@gmail.com>
2022-11-02 19:15:39 +0200

    lisää siivousta

--- components/PlayerSelect.tsx
+++ components/PlayerSelect.tsx
@@ -1,24 +1,8 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Player from '../types/Player';
-import { GetStaticProps } from "next"
-import prisma from '../lib/prisma';
-import Dashboard from "../components/Dashboard";
-import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import Grid from '@mui/material/Grid';
-import Title from '../components/Title';
-import Button from '@mui/material/Button';
-import Router from 'next/router';
-import GameResult from '../types/GameResult';
+import Select from '@mui/material/Select';
 
 const PlayerSelect = ({ players, title, handleChange }: { players: Player[], title: string, handleChange: any }) => {
     return (
--- pages/add-result.tsx
+++ pages/add-result.tsx
@@ -1,14 +1,8 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
 import Player from '../types/Player';
 import { GetStaticProps } from "next"
 import prisma from '../lib/prisma';
 import Dashboard from "../components/Dashboard";
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import GameResult from '../types/GameResult';
-import { SelectChangeEvent } from '@mui/material/Select';
-import TeamResult from '../components/TeamResult';
+import AddResult from '../components/AddResult'
 
 export const getStaticProps: GetStaticProps = async () => {
     const players = await prisma.player.findMany({
@@ -24,86 +18,6 @@ export const getStaticProps: GetStaticProps = async () => {
         props: { players }
     };
 };
-
-const AddResult = ({ players }: { players: Player[] }) => {
-    const [team1Player1Id, setTeam1Player1Id] = React.useState("");
-    const [team1Player2Id, setTeam1Player2Id] = React.useState("");
-    const [team1Points, setTeam1Points] = React.useState(0);
-    const [team2Player1Id, setTeam2Player1Id] = React.useState("");
-    const [team2Player2Id, setTeam2Player2Id] = React.useState("");
-    const [team2Points, setTeam2Points] = React.useState(0);
-
-    const team1Player1Change = (event: SelectChangeEvent) => {
-        setTeam1Player1Id(event.target.value);
-    }
-
-    const team1Player2Change = (event: SelectChangeEvent) => {
-        setTeam1Player2Id(event.target.value);
-    }
-
-    const team1PointsChange = (event: SelectChangeEvent) => {
-        setTeam1Points(Number(event.target.value));
-    }
-
-    const team2Player1Change = (event: SelectChangeEvent) => {
-        setTeam2Player1Id(event.target.value);
-    }
-
-    const team2Player2Change = (event: SelectChangeEvent) => {
-        setTeam2Player2Id(event.target.value);
-    }
-
-    const team2PointsChange = (event: SelectChangeEvent) => {
-        setTeam2Points(Number(event.target.value));
-    }
-
-    const handleButtonClick = async (e: React.SyntheticEvent) => {
-        e.preventDefault();
-
-        try {
-            const body: GameResult = {
-                team1Result: {
-                    player1Id: team1Player1Id,
-                    player2Id: team1Player2Id,
-                    points: team1Points
-                },
-                team2Result: {
-                    player1Id: team2Player1Id,
-                    player2Id: team2Player2Id,
-                    points: team2Points
-                }
-            };
-
-            await fetch('/api/add-result', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify(body),
-            });
-          } catch (error) {
-            console.error(error);
-          }
-    }
-
-    return (
-        <Grid container rowSpacing={2}>
-            <Grid item xs={12} pb={2}>
-                <Paper>
-                    <TeamResult players={players} title="Joukkue 1" player1HandleChange={team1Player1Change} player2HandleChange={team1Player2Change}
-                                pointsHandleChange={team1PointsChange} />
-                </Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper>
-                    <TeamResult players={players} title="Joukkue 2" player1HandleChange={team2Player1Change} player2HandleChange={team2Player2Change} 
-                                pointsHandleChange={team2PointsChange} />
-                </Paper>
-            </Grid>
-            <Grid item xs={12} container justifyContent="flex-end">
-                <Button variant="contained" onClick={handleButtonClick}>Lisää</Button>
-            </Grid>
-        </Grid>
-    )
-}
 
 const AddResultContainer = ({ players }: { players: Player[] }) => {
     return (
