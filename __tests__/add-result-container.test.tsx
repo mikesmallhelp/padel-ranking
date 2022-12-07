14ee43817d0dae1757aa9fb35259d41c463e03c1
Mike <mikesmallhelp@gmail.com>
2022-12-07 19:31:25 +0200

    Test the player and points selections

--- __tests__/add-result-container.test.tsx
+++ __tests__/add-result-container.test.tsx
@@ -4,35 +4,49 @@ import "@testing-library/jest-dom";
 import { players } from '../lib/tests-lib/mock-data';
 import { checkDashboard } from "../lib/tests-lib/common-test-utils";
 
-describe('index.tsx', () => {
-    it('check the add result container', () => {
+describe('add-result-container.tsx', () => {
+    it('check the dashboard', () => {
         render(<AddResultContainer players={players} />);
-
         checkDashboard({ dashboardTitle: "Lisää tulos" });
-        checkAddResultTitles();
+    })
 
-        //expect(screen.getByRole('link', { name: 'Pelaajatx' })).toBeInTheDocument();
-        expect(screen.getByTestId("playerSelectJoukkue 1Pelaaja 1")).toBeTruthy();
-        expect(screen.getByTestId("playerSelectJoukkue 1Pelaaja 1").childNodes.length).toBe(4);
-        //const user = userEvent.setup();
-        //expect(screen.getByTestId("playerNameSelectValueJoukkue 1Pelaaja 1Jarkko").textContent).toContain("Jarkko");
+    it('check the titles', () => {
+        render(<AddResultContainer players={players} />);
+        
+        expect(screen.getByTestId("teamResultTitleJoukkue 1").textContent).toContain("Joukkue 1");
+        expect(screen.getByTestId("teamResultTitleJoukkue 2").textContent).toContain("Joukkue 2");
+        
+        expect(screen.getByTestId("playerSelectTitleJoukkue 1Pelaaja 1").textContent).toContain("Pelaaja 1");
+        expect(screen.getByTestId("playerSelectTitleJoukkue 1Pelaaja 2").textContent).toContain("Pelaaja 2");
+        expect(screen.getByTestId("playerSelectTitleJoukkue 2Pelaaja 1").textContent).toContain("Pelaaja 1");
+        expect(screen.getByTestId("playerSelectTitleJoukkue 2Pelaaja 2").textContent).toContain("Pelaaja 2");
+    
+        expect(screen.getByTestId("pointsSelectTitleJoukkue 1Pisteet").textContent).toContain("Pisteet");
+        expect(screen.getByTestId("pointsSelectTitleJoukkue 2Pisteet").textContent).toContain("Pisteet");
+    })
 
-        fireEvent.mouseDown(screen.getAllByRole("button")[1]);
-        const listbox = within(screen.getByRole("listbox"));
-        fireEvent.click(listbox.getByText(/Jarkko/i));
-        expect(screen.getByTestId("playerSelectJoukkue 1Pelaaja 1")).toHaveTextContent(/Jarkko/i);
+    it('check the player selects', () => {
+        render(<AddResultContainer players={players} />);
+        [1, 2, 4, 5].forEach(buttonRoleIndex => testPlayerSelectsForAllPlayers({ buttonRoleIndex: buttonRoleIndex }));
+    })
+
+    it('check the points selects', () => {
+        render(<AddResultContainer players={players} />);
+        [3, 6].forEach(buttonRoleIndex => testPointsSelectsForAllPoints({ buttonRoleIndex: buttonRoleIndex }));
     })
 })
 
-const checkAddResultTitles = () => {
-    expect(screen.getByTestId("teamResultTitleJoukkue 1").textContent).toContain("Joukkue 1");
-    expect(screen.getByTestId("teamResultTitleJoukkue 2").textContent).toContain("Joukkue 2");
-    
-    expect(screen.getByTestId("playerSelectTitleJoukkue 1Pelaaja 1").textContent).toContain("Pelaaja 1");
-    expect(screen.getByTestId("playerSelectTitleJoukkue 1Pelaaja 2").textContent).toContain("Pelaaja 2");
-    expect(screen.getByTestId("playerSelectTitleJoukkue 2Pelaaja 1").textContent).toContain("Pelaaja 1");
-    expect(screen.getByTestId("playerSelectTitleJoukkue 2Pelaaja 2").textContent).toContain("Pelaaja 2");
+const testPlayerSelectsForAllPlayers = ({ buttonRoleIndex }: { buttonRoleIndex: number }) => {
+    ["Jarkko", "Joonas", "Mika", "Tommi", "Ville"].forEach(playerName => testSelect({buttonRoleIndex: buttonRoleIndex, menuItemText: playerName}));
+}
 
-    expect(screen.getByTestId("pointsSelectTitleJoukkue 1Pisteet").textContent).toContain("Pisteet");
-    expect(screen.getByTestId("pointsSelectTitleJoukkue 2Pisteet").textContent).toContain("Pisteet");
+const testPointsSelectsForAllPoints = ({ buttonRoleIndex }: { buttonRoleIndex: number }) => {
+    ["0", "1", "2", "3", "4", "5", "6"].forEach(pointsValue => testSelect({buttonRoleIndex: buttonRoleIndex, menuItemText: pointsValue}));
+}
+
+const testSelect = ({ buttonRoleIndex, menuItemText}: { buttonRoleIndex: number, menuItemText: string }) => {
+    fireEvent.mouseDown(screen.getAllByRole("button")[buttonRoleIndex]);
+    const listbox = within(screen.getByRole("listbox"));
+    fireEvent.click(listbox.getByText(menuItemText));
+    expect(screen.getAllByRole("button")[buttonRoleIndex].textContent).toEqual(menuItemText);
 }
