ba8fff21d73fc06d4cc3c91cb61f02fafb47af09
Mike <mikesmallhelp@gmail.com>
2022-12-13 17:41:19 +0200

    Check element content on page

--- tests/padel-games.spec.ts
+++ tests/padel-games.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('the padel games smoke test', async ({ page }) => {
   await page.goto('https://padel-ranking.vercel.app/');
 
-  expect(await page.locator('data-testid=dashboardTitle').count()).toBeTruthy();
+  expect(await page.locator('data-testid=dashboardTitle').textContent()).toBe("Padel-pelit");
 });
