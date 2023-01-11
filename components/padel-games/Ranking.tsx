a97be66f5abaf30aad0dfa34f43da2a5b779e803
Mike <mikesmallhelp@gmail.com>
2023-01-11 19:19:33 +0200

    Add .env for local development

--- components/padel-games/Ranking.tsx
+++ components/padel-games/Ranking.tsx
@@ -10,7 +10,7 @@ import Player from "../../types/Player";
 const Ranking = ({ players }: { players: Player[] }) => {
   return (
     <React.Fragment>
-      <Title titleTestId="rankingTitle">Ranking 11.1.2023 18:30</Title>
+      <Title titleTestId="rankingTitle">Ranking</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
