0f93b69b4caaf70d26ab425ae80f56f54303eabf
Mike <mikesmallhelp@gmail.com>
2023-01-24 17:59:56 +0200

    Test the ranking

--- tests/e2e/padel-games.test.ts
+++ tests/e2e/padel-games.test.ts
@@ -1,7 +1,16 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
-test("the padel games smoke test", async ({ page }) => {
+test("test the ranking page", async ({ page }) => {
   await page.goto("https://padel-ranking.vercel.app/");
-
-  expect(await page.locator("data-testid=dashboardTitle").textContent()).toBe("Padel-pelit");
+  await checkPlayerRanking({page: page, playerName: "Tommi", games: "2", points: "12"});
+  await checkPlayerRanking({page: page, playerName: "Ville", games: "2", points: "12"});
+  await checkPlayerRanking({page: page, playerName: "Jarkko", games: "2", points: "3"});
+  await checkPlayerRanking({page: page, playerName: "Joonas", games: "2", points: "3"});
+  await checkPlayerRanking({page: page, playerName: "Mika", games: "0", points: "0"});
 });
+
+const checkPlayerRanking = async ({ page, playerName, games, points }: {page: Page, playerName: string, games: string, points: string}) => {
+  expect(await page.locator("data-testid=id" + playerName + "Name").textContent()).toContain(playerName);
+  expect(await page.locator("data-testid=id" + playerName + "Games").textContent()).toContain(games);
+  expect(await page.locator("data-testid=id" + playerName + "Points").textContent()).toContain(points);
+}
