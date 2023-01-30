17de7379155a820756861de54a43f0b673caf732
Mike <mikesmallhelp@gmail.com>
2023-01-30 19:28:39 +0200

    Add the game result tests

--- tests/e2e/padel-games.test.ts
+++ tests/e2e/padel-games.test.ts
@@ -1,6 +1,6 @@
 import { test, expect, Page } from "@playwright/test";
 
-test("test the ranking page", async ({ page, baseURL }) => {
+test("test the player rankings", async ({ page, baseURL }) => {
   if (!baseURL) {
     throw Error("The base url not defined!");
   }
@@ -13,8 +13,31 @@ test("test the ranking page", async ({ page, baseURL }) => {
   await checkPlayerRanking({page: page, playerName: "Mika", games: "0", points: "0"});
 });
 
+test("test the game results", async ({ page, baseURL }) => {
+  if (!baseURL) {
+    throw Error("The base url not defined!");
+  }
+  
+  await page.goto(baseURL);
+  // Not testing times, because couldn't easily create data with the correct time zone. So at winter time created tests not possibly work
+  // at summer time.
+  await checkGameResult({page: page, gameNumber: 1, createdAt: "08.11.2022", team1: "Tommi & Ville", team2: "Jarkko & Joonas", result: "6 - 1"});
+  await checkGameResult({page: page, gameNumber: 2, createdAt: "09.11.2022", team1: "Tommi & Ville", team2: "Jarkko & Joonas", result: "6 - 2"});
+});
+
 const checkPlayerRanking = async ({ page, playerName, games, points }: {page: Page, playerName: string, games: string, points: string}) => {
   expect(await page.locator("data-testid=id" + playerName + "Name").textContent()).toContain(playerName);
   expect(await page.locator("data-testid=id" + playerName + "Games").textContent()).toContain(games);
   expect(await page.locator("data-testid=id" + playerName + "Points").textContent()).toContain(points);
+}
+
+const checkGameResult = async ({ page, gameNumber, createdAt, team1, team2, result }: {
+  page: Page, gameNumber: number, createdAt: string, team1: string,
+  team2: string, result: string
+}) => {
+  const createdAtFromPage = await page.locator("data-testid=gr" + gameNumber + "CreatedAt").textContent()
+  expect(createdAtFromPage?.substring(0, 10)).toContain(createdAt);
+  expect(await page.locator("data-testid=gr" + gameNumber + "Team1").textContent()).toContain(team1);
+  expect(await page.locator("data-testid=gr" + gameNumber + "Team2").textContent()).toContain(team2);
+  expect(await page.locator("data-testid=gr" + gameNumber + "Result").textContent()).toContain(result);
 }
