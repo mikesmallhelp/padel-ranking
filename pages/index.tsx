11ae835abd52b1aa14f3bba6bc75f2472766f615
Mike <mikesmallhelp@gmail.com>
2023-04-17 18:31:55 +0300

    Login link

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,19 +52,24 @@ const PadelGamesContainer = ({ players, gameResults }: { players: Player[], game
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  return (
-    <>
+  if (!user) {
+    return (
       <a href="/api/auth/login">Login</a>
-      <br></br>
-      <a href="/api/auth/logout">Logout</a>
-      <br></br>
-      <h2>{user?.name}</h2>
-      <p>{user?.email}</p>
-      <Dashboard title="Padel-pelit">
-        <PadelGames players={players} gameResults={gameResults} />
-      </Dashboard>
-    </>
-  )
+    )
+  } else {
+    return (
+      <>
+        <br></br>
+        <a href="/api/auth/logout">Logout</a>
+        <br></br>
+        <h2>{user?.name}</h2>
+        <p>{user?.email}</p>
+        <Dashboard title="Padel-pelit">
+          <PadelGames players={players} gameResults={gameResults} />
+        </Dashboard>
+      </>
+    )
+  }
 }
 
 export default PadelGamesContainer;
