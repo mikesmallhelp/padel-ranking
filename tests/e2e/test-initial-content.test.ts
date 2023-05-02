17414582b5a978ec0fd9efbb01fe95eaed4d5e0c
Mike <mikesmallhelp@gmail.com>
2023-05-02 18:35:01 +0300

    Simplify the initial content test also

--- tests/e2e/test-initial-content.test.ts
+++ tests/e2e/test-initial-content.test.ts
@@ -1,13 +1,8 @@
-import { test } from "@playwright/test";
+import { test, Page } from "@playwright/test";
 import { checkPlayerRanking, checkGameResult, checkPlayer, authenticate } from "./test-utils";
 
 test("test the player rankings", async ({ page, baseURL }) => {
-  if (!baseURL) {
-    throw Error("The base url not defined!");
-  }
-  
-  await page.goto(baseURL);
-  await authenticate({page: page, password: process.env.E2E_TEST_PASSWORD});
+  await checkBaseUrlAndDoAuthentication({ page: page, baseURL: baseURL, contextPath: "" });
 
   await checkPlayerRanking({page: page, playerName: "Tommi", games: "2", points: "12"});
   await checkPlayerRanking({page: page, playerName: "Ville", games: "2", points: "12"});
@@ -17,24 +12,14 @@ test("test the player rankings", async ({ page, baseURL }) => {
 });
 
 test("test the game results", async ({ page, baseURL }) => {
-  if (!baseURL) {
-    throw Error("The base url not defined!");
-  }
-  
-  await page.goto(baseURL);
-  await authenticate({page: page, password: process.env.E2E_TEST_PASSWORD});
+  await checkBaseUrlAndDoAuthentication({ page: page, baseURL: baseURL, contextPath: "" });
 
   await checkGameResult({page: page, createdAt: "08.11.2022", team1: "Tommi & Ville", team2: "Jarkko & Joonas", result: "6 - 1"});
   await checkGameResult({page: page, createdAt: "09.11.2022", team1: "Tommi & Ville", team2: "Jarkko & Joonas", result: "6 - 2"});
 });
 
 test("test the players", async ({ page, baseURL }) => {
-  if (!baseURL) {
-    throw Error("The base url not defined!");
-  }
-  
-  await page.goto(baseURL + "players-container");
-  await authenticate({page: page, password: process.env.E2E_TEST_PASSWORD});
+  await checkBaseUrlAndDoAuthentication({ page: page, baseURL: baseURL, contextPath: "players-container" });
 
   await checkPlayer({page: page, playerName: "Jarkko"});
   await checkPlayer({page: page, playerName: "Joonas"});
@@ -42,3 +27,12 @@ test("test the players", async ({ page, baseURL }) => {
   await checkPlayer({page: page, playerName: "Tommi"});
   await checkPlayer({page: page, playerName: "Ville"});
 });
+
+const checkBaseUrlAndDoAuthentication = async ({ page, baseURL, contextPath }: { page: Page, baseURL?: string, contextPath: string }) => {
+  if (!baseURL) {
+    throw Error("The base url not defined!");
+  }
+
+  await page.goto(baseURL + contextPath);
+  await authenticate({ page: page, password: process.env.E2E_TEST_PASSWORD });
+}
