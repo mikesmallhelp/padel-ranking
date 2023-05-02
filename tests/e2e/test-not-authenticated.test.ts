08a46649c61512f016c9b476f32aa287240741ba
Mike <mikesmallhelp@gmail.com>
2023-05-02 17:51:01 +0300

    Simplify the tests

--- tests/e2e/test-not-authenticated.test.ts
+++ tests/e2e/test-not-authenticated.test.ts
@@ -1,33 +1,18 @@
 import { test, expect, Page } from "@playwright/test";
 
 test("test the not authenticated base page", async ({ page, baseURL }) => {
-  if (!baseURL) {
-    throw Error("The base url not defined!");
-  }
-  
-  await page.goto(baseURL);
-  await expect(page.getByText("Log in")).toBeVisible();
+  await doTest({page: page, baseURL: baseURL, contextPath: ""});
 });
 
 test("test the not authenticated add result page", async ({ page, baseURL }) => {
-  if (!baseURL) {
-    throw Error("The base url not defined!");
-  }
-  
-  await page.goto(baseURL + "add-result-container");
-  await expect(page.getByText("Log in")).toBeVisible();
+  await doTest({page: page, baseURL: baseURL, contextPath: "add-result-container"});
 });
 
 test("test the not authenticated players page", async ({ page, baseURL }) => {
-  if (!baseURL) {
-    throw Error("The base url not defined!");
-  }
-  
-  await page.goto(baseURL + "players-container");
-  await expect(page.getByText("Log in")).toBeVisible();
+  await doTest({page: page, baseURL: baseURL, contextPath: "players-container"});
 });
 
-const doTest = async ({ page, baseURL, contextPath} : { page: Page, baseURL: string, contextPath: string }) => {
+const doTest = async ({ page, baseURL, contextPath} : { page: Page, baseURL?: string, contextPath: string }) => {
   if (!baseURL) {
     throw Error("The base url not defined!");
   }
