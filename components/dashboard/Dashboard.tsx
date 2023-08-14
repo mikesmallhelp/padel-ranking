54e46647244f929862ea804c39e314e43fd028cc
Mike <mikesmallhelp@gmail.com>
2023-08-14 17:18:37 +0300

    Remove copyright link

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -19,9 +19,7 @@ function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {"Copyright © "}
-      <Link color="inherit" href="https://github.com/mikesmallhelp/">
         mikesmallhelp
-      </Link>{" "}
       {new Date().getFullYear()}
       {"."}
     </Typography>
